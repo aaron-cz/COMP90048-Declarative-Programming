d3f72dddd172bef6790672791030b72cab910158
Owen Zhu <hanweiz@student.unimelb.edu.au>
2017-08-04 12:47:45 +1000

    try to use Guards in 2.hs

--- scripts/workshop1/2.hs
+++ scripts/workshop1/2.hs
@@ -1,9 +1,13 @@
 -- Implement a function to perform the logical XOR
 xor :: Bool -> Bool -> Bool
-
+{-
 xor x y =
     if x /= y
     then
         True
     else
         False
+-}
+xor x y 
+    |  x /= y = True
+    |  otherwise = False
